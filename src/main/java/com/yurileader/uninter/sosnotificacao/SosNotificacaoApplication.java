5ad2b478677acf2ac1e5b13727eabbcb7e577976
Yuri <yurileader@gmail.com>
2024-12-01 12:19:40 -0300

    Estrututa inicial

---
+++ src/main/java/com/yurileader/uninter/sosnotificacao/SosNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.yurileader.uninter.sosnotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosNotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurileader/uninter/sosnotificacao/SosNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurileader.uninter.sosnotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
